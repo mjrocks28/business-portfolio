83052767f751f5f810372942654347dfae885cc6
mjrocks28 <76646335+mjrocks28@users.noreply.github.com>
2021-01-09 17:19:42 +0800

    added content on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,17 @@ import React from "react";
 import "../css/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="home-contents">
+        <div className="home-title">
+          <h1 className="title">Begin PH</h1>
+          <span className="subtitle">Web Solutions</span>
+        </div>
+        <button>Contact Us</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
