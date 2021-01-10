e1e4ec18ac280151bcfaa879dfb12381237a1dcc
mjrocks28 <76646335+mjrocks28@users.noreply.github.com>
2021-01-10 16:21:02 +0800

    added mobile responsive on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 import "../css/Navbar.css";
 
@@ -7,50 +7,106 @@ const Navbar = () => {
     scroll.scrollToTop();
   };
 
-  return (
-    <div className="navbar">
-      <a onClick={scrollToTop} className="logo">
-        Begin PH
-      </a>
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-      <ul className="nav-items">
-        <li>
-          <Link
-            to="about"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="services"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-          >
-            Services
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="contact"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-20}
-            duration={500}
-          >
-            Contact Us
-          </Link>
-        </li>
-      </ul>
+  return (
+    <div>
+      <div className="navbar">
+        <a href="/#" onClick={scrollToTop} className="logo">
+          Begin PH
+        </a>
+
+        <a href="/#" className="toggle-button" onClick={handleClick}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </a>
+
+        <ul className="nav-items">
+          <li>
+            <Link
+              to="about"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="services"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="contact"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-20}
+              duration={500}
+            >
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className={click ? "mobile-navbar mobile-active" : "mobile-navbar"}>
+        <ul className="mobile-nav-items">
+          <li>
+            <Link
+              to="about"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={closeMobileMenu}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="services"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={closeMobileMenu}
+            >
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="contact"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-20}
+              duration={500}
+              onClick={closeMobileMenu}
+            >
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
