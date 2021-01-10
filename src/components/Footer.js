f8c2bcfebd95ed3fdb045d9dd54c9a019d15beeb
mjrocks28 <76646335+mjrocks28@users.noreply.github.com>
2021-01-10 21:29:22 +0800

    added footer contents

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,52 @@
 import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
+import { SocialIcon } from "react-social-icons";
 import "../css/Footer.css";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-brand">
+          <h1>Begin PH</h1>
+          <span>Web Solutions</span>
+        </div>
+
+        <div className="footer-company">
+          <p>Company</p>
+
+          <ul>
+            <li>
+              <Link to="about" smooth={true} offset={-70} duration={500}>
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link to="contact" smooth={true} offset={-70} duration={500}>
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="socials">
+          <p className="socials-title">Socials</p>
+
+          <div className="socials-container">
+            <SocialIcon url="https://facebook.com" />
+            <SocialIcon url="https://instagram.com" />
+            <SocialIcon network="email" url="mj28_mendoza@icloud.com" />
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <span>&copy; Begin PH | Web Solutions 2021.</span>
+        <span>Privacy Statement</span>
+        <span>Terms of Use</span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
