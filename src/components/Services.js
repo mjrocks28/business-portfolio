779009e1ed641a74f8747663a98784363900dcd8
mjrocks28 <76646335+mjrocks28@users.noreply.github.com>
2021-02-01 20:04:10 +0800

    arranged services contents

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,62 +5,73 @@ function Services() {
   return (
     <div className="services" id="services">
       <div className="services-title-container">
-          <h1 className="services-title">Services</h1>
+        <h1 className="services-title">Services</h1>
       </div>
-    
+
       <div className="services-contents">
-      <div class="row">
-          <div class="column ">
-            <div class="card-pw">
-            <div class="card-header">            
-               <h4>Personal Website</h4>
-            </div>          
+        <div className="column ">
+          <div className="card-pw">
+            <div className="card-header">
+              <h4>Personal Website</h4>
+            </div>
             <p>Hosting and Domain for 1 Year</p>
             <p>Automated Emails</p>
-            </div>
+            <p>Static Pages</p>
+            <p>Mobile View Friendly</p>
+            <p>Login/Registration</p>
           </div>
+        </div>
 
-          <div class="column">
-          <div class="card-bw">
-              <div class="card-header">            
-               <h4>Business Website</h4>
-              </div>   
-              <p>Hosting and Domain for 1 Year</p>
-              <p>Customizable Website</p>
-              <p>Admin Panel</p>
-              <p>Automated Emails</p>
+        <div className="column">
+          <div className="card-bw">
+            <div className="card-header">
+              <h4>Business Website</h4>
             </div>
+            <p>Hosting and Domain for 1 Year</p>
+            <p>Customizable Website</p>
+            <p>Cloud Database</p>
+            <p>Admin Panel</p>
+            <p>Automated Emails</p>
+            <p>Dynamic Pages</p>
+            <p>Mobile View Friendly</p>
+            <p>Login/Registration</p>
           </div>
-          
-          <div class="column">
-          <div class="card-ecs">
-              <div class="card-header">            
-                <h4>E-Commerce (Simple)</h4>          
-              </div>            
-              <p>Hosting and Domain for 1 Year</p>
-              <p>Cloud Database</p>
-              <p>Admin Panel</p>
-              <p>Automated Emails</p>
-              <p>Payment Options</p>
-            </div>
-          </div>
+        </div>
 
-          <div class="column">
-          <div class="card-eca">
-              <div class="card-header">            
-                <h4>E-Commerce (Advanced)</h4>
-              </div> 
-              <p>All inclusions of Simple Version</p>
-              <p>Support/Ticketing Panel</p>
+        <div className="column">
+          <div className="card-ecs">
+            <div className="card-header">
+              <h4>E-Commerce (Simple)</h4>
             </div>
+            <p>Hosting and Domain for 1 Year</p>
+            <p>Cloud Database</p>
+            <p>Admin Panel</p>
+            <p>Automated Emails</p>
+            <p>Payment Options</p>
+            <p>Dynamic Pages</p>
+            <p>Mobile View Friendly</p>
+            <p>Login/Registration</p>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card-eca">
+            <div className="card-header">
+              <h4>E-Commerce (Advanced)</h4>
+            </div>
+            <p>Hosting and Domain for 1 Year</p>
+            <p>Cloud Database</p>
+            <p>Admin Panel</p>
+            <p>Automated Emails</p>
+            <p>Payment Options</p>
+            <p>Dynamic Pages</p>
+            <p>Mobile View Friendly</p>
+            <p>Login/Registration</p>
+            <p>Support/Ticketing Panel</p>
           </div>
         </div>
       </div>
-
-   </div>
-  
-
-    
+    </div>
   );
 }
 
